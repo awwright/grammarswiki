7ac210337bc2e29623ca94c616b1e4d489192ebb
Austin Wright <aaa@bzfx.net>
2025-03-04 23:47:51 -0700

    Add Xcode workspace and simple command-line tool

---
+++ grammartool/grammartool/main.swift
@@ -0,0 +1,58 @@
+import Foundation;
+import FSM;
+
+let arguments = CommandLine.arguments
+//guard arguments.count > 1 else {
+//	print("Usage: \(arguments[0]) <filename>")
+//	print("Also accepts stdin input after file processing (Ctrl+D or Ctrl+Z to end)")
+//	exit(1)
+//}
+print(arguments);
+
+func getInput() -> Data? {
+	// Check command-line arguments
+	let filename: String?
+
+	if arguments.count > 1 {
+		filename = arguments[1]
+	} else {
+		filename = nil
+	}
+
+	if let filename {
+		// Read raw bytes from file
+		do {
+			let fileURL = URL(fileURLWithPath: filename)
+			return try Data(contentsOf: fileURL)
+		} catch {
+			print("Error reading file '\(filename)': \(error)")
+			return nil
+		}
+	} else {
+		// Read raw bytes from stdin
+		let data = FileHandle.standardInput.availableData
+		return data.isEmpty ? nil : data
+	}
+}
+
+typealias Char = UInt32;
+
+//print(getInput()/*!*/);
+let rulelist = ABNFRulelist<Char>.parse(getInput()!)!;
+let parsedRules = rulelist.toPattern(DFA<Array<Char>>.self, rules: [:])
+print(parsedRules.keys);
+//print(rulelist.description);
+
+guard arguments.count >= 3 else {
+	exit(0);
+}
+
+let rulename = arguments[2]
+guard let fsm = parsedRules[rulename] else {
+	print("Could not compile \(rulename)");
+	exit(1);
+}
+print(fsm.toViz());
+var pattern = fsm.toPattern(SimpleRegex<Char>.self)
+print(pattern.description)
+print(pattern.toPattern(ABNFAlternation<Char>.self).description)
