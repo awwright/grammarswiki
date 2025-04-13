797f4b36e3b4f95e244acb9603bec09f70248eaf
Austin Wright <aaa@bzfx.net>
2025-04-12 19:48:03 -0700

    Swift: SymbolClass: Add a struct that represents an alphabet and tracks partitions of equivalent symbols

--- FSM/lib/SymbolClass.swift
+++ FSM/lib/SymbolClass.swift
@@ -1,3 +1,273 @@
+/// A set of symbols, with tracking equivalency of elements (placing symbols with the same behavior in the same partition)
+/// Optimized for describing ranges of characters.
+/// A replacement for Swift's builtin RangeSet which doesn't support ClosedRange and is overall garbage
+public struct SymbolClass<Symbol: Comparable & Hashable>: ExpressibleByArrayLiteral, SetAlgebra where Symbol: BinaryInteger {
+	// MARK: Type definitions
+	public typealias ArrayLiteralElement = Symbol
+	public typealias Element = Symbol
+
+	// MARK: Properties
+	// Ordered list of all symbols (and ranges) in the class
+	var symbols: Array<ClosedRange<Symbol>>
+	// A tree partitioning elements together
+	var parents: Array<Int>
+
+	// MARK: Initializations
+	public init() {
+		self.symbols = []
+		self.parents = []
+	}
+
+	public init(symbols: Array<ClosedRange<Symbol>>, parents: Array<Int>) {
+		self.symbols = symbols
+		self.parents = parents
+	}
+
+	public init(partitions: Array<Array<Symbol>>) {
+		self = Self.meet(partitions.compactMap {
+			if($0.isEmpty){ return nil }
+			let sorted = $0.sorted();
+			return SymbolClass(symbols: sorted.map{ $0...$0 }, parents: Array(repeating: 0, count: sorted.count))
+		})
+	}
+
+	public init(_ partitions: Array<Symbol>...) {
+		let union = partitions.flatMap { $0 }
+		self.symbols = union.map { $0...$0 }.sorted { $0.lowerBound < $1.lowerBound }
+		self.parents = self.symbols.enumerated().map { i, _ in i }
+		for part in partitions {
+			if part.isEmpty { continue }
+			let partSorted = part.sorted { $0 < $1 }
+			let partLabel = partSorted[0]
+			let partLabelIndex = findIndex(partLabel)!
+			for i in 0..<partSorted.count {
+				self.parents[findIndex(partSorted[i])!] = partLabelIndex
+			}
+		}
+	}
+
+	public init(partitions: Array<Array<ClosedRange<Symbol>>>) {
+		let union = partitions.flatMap { $0 }
+		self.symbols = union.sorted { $0.lowerBound < $1.lowerBound }
+		self.parents = self.symbols.enumerated().map { i, _ in i }
+		for part in partitions {
+			if part.isEmpty { continue }
+			let partSorted = part.sorted { $0.lowerBound < $1.lowerBound }
+			let partLabel = partSorted[0].lowerBound
+			let partLabelIndex = findIndex(partLabel)!
+			for i in 0..<partSorted.count {
+				self.parents[findIndex(partSorted[i].lowerBound)!] = partLabelIndex
+			}
+		}
+	}
+
+	public init(_ partitions: Array<ClosedRange<Symbol>>...) {
+		self.init(partitions: partitions)
+	}
+
+	// MARK: ExpressibleByArrayLiteral
+	public init(arrayLiteral elements: ArrayLiteralElement...) {
+		self.symbols = elements.map { $0...$0 }.sorted { $0.lowerBound < $1.lowerBound }
+		self.parents = self.symbols.enumerated().map { i, _ in i }
+	}
+
+	public var partitionLabels: Array<Symbol> {
+		self.parents.filter { $0 == parents[$0] }.map{ symbols[$0].lowerBound }
+	}
+
+	/// Computes a mapping of symbol to partition label
+	public var alphabetReduce: Dictionary<Symbol, Symbol> {
+		return [:]
+	}
+
+	/// Maps partition label to set of values in partition
+	public var alphabetExpand: Dictionary<Symbol, Set<Symbol>> {
+		return [:]
+	}
+
+	public var partitionCount: Int {
+		Set(parents).count
+	}
+
+	public func isEquivalent(_ lhs: Symbol, _ rhs: Symbol) -> Bool {
+		func find(_ symbol: Symbol) -> Int {
+			var index = self.findFirstIndex(symbol)
+			assert(self.symbols[index].contains(symbol));
+			while index != self.parents[index] {
+				index = self.parents[index]
+			}
+			return index
+		}
+		return find(lhs) == find(rhs)
+	}
+
+	public func getPartitionLabel(_ symbol: Symbol) -> Symbol {
+		let index = self.findIndex(symbol)
+		guard let index else { fatalError() }
+		return self.symbols[self.parents[index]].lowerBound
+	}
+
+	// MARK: SetAlgebra
+	public func contains(_ member: Symbol) -> Bool {
+		// Binary search `symbols` for a matching symbol
+		var lower = self.symbols.startIndex
+		var upper = self.symbols.endIndex
+		while lower != upper {
+			let middle = (lower + upper) / 2
+			let node = self.symbols[middle]
+			if node.upperBound < member {
+				lower = middle + 1
+			} else if member < node.lowerBound {
+				upper = middle
+			} else if node.lowerBound <= member && member <= node.upperBound {
+				return true
+			}
+		}
+		return false
+	}
+
+	/// Find the union of the elements with `other`, but divide partitions along their intersections
+	public static func meet(_ other: Array<SymbolClass<Symbol>>) -> SymbolClass<Symbol> {
+		if other.isEmpty {
+			return Self()
+		} else if other.count == 1 {
+			return other[0]
+		}
+
+		// Compute the intersection of all symbols (ranges)
+		let allSymbols = other.flatMap { $0.symbols }.sorted { $0.lowerBound < $1.lowerBound }
+		var lowerBounds: Array<Symbol> = allSymbols.map(\.lowerBound).sorted();
+		var upperBounds: Array<Symbol> = allSymbols.map(\.upperBound).sorted();
+		assert(lowerBounds.count == upperBounds.count)
+
+		// Un-nest ranges by adding lower or upper bounds as necessary
+		// Only add bounds inside one range or another, test this by counting how many bounds are open
+		let nestedUpperBounds: Array<Symbol> = lowerBounds.compactMap {
+			offset in
+			if (lowerBounds.filter { $0 < offset }.count > upperBounds.filter { $0 < offset }.count) { return offset-1 }
+			else { return nil }
+		};
+		let nestedLowerBounds: Array<Symbol> = upperBounds.compactMap {
+			offset in
+			if (lowerBounds.filter { $0 <= offset }.count >= upperBounds.filter { $0 <= offset }.count + 1) { return offset+1 }
+			else { return nil }
+		};
+		lowerBounds = Set(lowerBounds+nestedLowerBounds).sorted()
+		upperBounds = Set(upperBounds+nestedUpperBounds).sorted()
+		assert(lowerBounds.count == upperBounds.count)
+		let symbols: [ClosedRange<Symbol>] = zip(lowerBounds, upperBounds).map { $0...$1 }
+
+		// Assign each range in `symbols` to a new partition
+		let parts: Array<Array<Symbol?>> = symbols.map {
+			subrange in
+			other.map { other in
+				let index = other.symbols.firstIndex(where: { $0.contains(subrange.lowerBound) })
+				guard let index else { return nil }
+				return other.symbols[other.parents[index]].lowerBound
+			}
+		}
+		// For the new partition index, just point to the first range to exist in the same set of partitions
+		let parents = parts.map {
+			parts.firstIndex(of: $0)!
+		}
+		return SymbolClass(
+			symbols: symbols,
+			parents: parents
+		)
+	}
+	public func meet(_ other: __owned SymbolClass<Symbol>) -> SymbolClass<Symbol> {
+		return Self.meet([self, other])
+	}
+
+	public func union(_ other: __owned SymbolClass<Symbol>) -> SymbolClass<Symbol> {
+		fatalError("Unimplemented")
+	}
+
+	public func intersection(_ other: SymbolClass<Symbol>) -> SymbolClass<Symbol> {
+		fatalError("Unimplemented")
+	}
+
+	public func symmetricDifference(_ other: __owned SymbolClass<Symbol>) -> SymbolClass<Symbol> {
+		fatalError("Unimplemented")
+	}
+
+	public mutating func formUnion(_ other: __owned SymbolClass<Symbol>) {
+		self = self.union(other)
+	}
+
+	public mutating func formIntersection(_ other: SymbolClass<Symbol>) {
+		self = self.intersection(other)
+	}
+
+	public mutating func formSymmetricDifference(_ other: __owned SymbolClass<Symbol>) {
+		self = self.symmetricDifference(other)
+	}
+
+	public mutating func insert(_ newMember: __owned Symbol) -> (inserted: Bool, memberAfterInsert: Symbol) {
+		let range = ClosedRange(uncheckedBounds: (newMember, newMember))
+		// Find insertion point
+		let insertionIndex = symbols.firstIndex(where: { $0.lowerBound > newMember }) ?? symbols.count
+		symbols.insert(range, at: insertionIndex)
+		symbols.insert(range, at: insertionIndex)
+		return (true, newMember)
+	}
+
+	public mutating func remove(_ member: Symbol) -> Symbol? {
+		guard let index = symbols.firstIndex(where: { $0.contains(member) }) else {
+			return nil
+		}
+		symbols.remove(at: index)
+		parents.remove(at: index)
+		return member
+	}
+
+	public mutating func update(with newMember: __owned Symbol) -> Symbol? {
+		let (inserted, _) = insert(newMember)
+		return inserted ? nil : newMember
+	}
+
+	public static func == (lhs: SymbolClass<Symbol>, rhs: SymbolClass<Symbol>) -> Bool {
+		return lhs.symbols == rhs.symbols
+	}
+
+	// MARK: Helpers
+
+	/// Finds the lowest index at or after the given symbol
+	private func findIndex(_ symbol: Symbol) -> Int? {
+		var lowerBound = 0
+		var upperBound = symbols.count - 1
+		while lowerBound <= upperBound {
+			let midIndex = (lowerBound + upperBound) / 2
+			let midRange = symbols[midIndex]
+			if midRange.contains(symbol) {
+				return midIndex
+			} else if symbol < midRange.lowerBound {
+				upperBound = midIndex - 1
+			} else if midRange.upperBound < symbol {
+				lowerBound = midIndex + 1
+			}
+		}
+		return nil
+	}
+
+	private func findFirstIndex(_ symbol: Symbol) -> Int {
+		var lowerBound = 0
+		var upperBound = symbols.count - 1
+		while lowerBound <= upperBound {
+			let midIndex = (lowerBound + upperBound) / 2
+			let midRange = symbols[midIndex]
+			if midRange.contains(symbol) {
+				return midIndex
+			} else if symbol < midRange.lowerBound {
+				upperBound = midIndex - 1
+			} else if midRange.upperBound < symbol {
+				lowerBound = midIndex + 1
+			}
+		}
+		return lowerBound
+	}
+}
+
 /// Find the partition-intersection of multiple sets, i.e. a set partitioned into the largest possible subsets that are always in the same input sets
 func partitionReduce<Symbol>(_ base: Set<Set<Symbol>>, _ with: Set<Symbol>) -> Set<Set<Symbol>> where Symbol: Hashable {
 	let extra = with.subtracting(Set(base.flatMap { $0 }))
@@ -26,6 +296,7 @@ public func compressPartitions<Symbol>(_ partitions: Set<Set<Symbol>>) -> (reduc
 	return (reduce, expand.mapValues{$0.sorted()}, alphabet)
 }
 
+/// Transparently maps a regular language with a large alphabet onto a DFA with a smaller alphabet where some symbols are equivalent
 public struct SymbolClassDFA<Symbol: Comparable & Hashable>: Sequence, Equatable, FSMProtocol {
 	public typealias StateNo = DFA<Symbol>.StateNo
 	public typealias States = DFA<Symbol>.States
--- FSM/test/SymbolClass.test.swift
+++ FSM/test/SymbolClass.test.swift
@@ -2,7 +2,7 @@ import Testing;
 @testable import FSM
 
 // Test Suite
-@Suite("SymbolClass") struct SymbolClassTests {
+@Suite("SymbolClass functions") struct SymbolClassFunctionsTests {
 	@Suite("partitionReduce") struct SymbolClassTests_partitionReduce {
 		@Test("empty") func test_partitionReduce_empty() async throws {
 			let alphabets = [
@@ -42,6 +42,228 @@ import Testing;
 			];
 			let partitions = alphabetCombine(alphabets);
 			#expect(partitions == Set([ Set("0"..."1"), Set("2"..."9"), Set("a"..."f"), Set("g"..."z") ]))
+		}
+	}
+}
+
+@Suite("SymbolClass") struct SymbolClassTests {
+	@Suite("contains") struct SymbolClassTests_partitionReduce {
+		@Test("array literal") func test_array_literal() async throws {
+			let partitions: SymbolClass<Int> = [0, 1]
+			#expect(partitions.contains(0))
+			#expect(partitions.contains(1))
+			#expect(!partitions.contains(2))
+		}
+		@Test("init ranges") func test_partitionReduce_empty() async throws {
+			let partitions: SymbolClass<Int> = []
+			#expect(!partitions.contains(1))
+			#expect(!partitions.contains(2))
+		}
+		@Test("single symbol") func test_partitionReduce_single() async throws {
+			let partitions = SymbolClass<Int>([ 1 ])
+			#expect(partitions.contains(1))
+			#expect(!partitions.contains(2))
+		}
+		@Test("single set multi-symbol") func test_partitionReduce_multi() async throws {
+			let partitions = SymbolClass<Int>([0, 1, 2])
+			#expect(partitions.contains(0))
+			#expect(partitions.contains(1))
+			#expect(partitions.contains(2))
+			#expect(!partitions.contains(3))
+		}
+		@Test("range of values") func test_contains_range() async throws {
+			let partitions = SymbolClass<Int>(0x30...0x39)
+			#expect(!partitions.contains(0x2F))
+			#expect(partitions.contains(0x30))
+			#expect(partitions.contains(0x31))
+			#expect(partitions.contains(0x38))
+			#expect(partitions.contains(0x39))
+			#expect(!partitions.contains(0x3A))
+		}
+		@Test("two ranges of values") func test_contains_ranges() async throws {
+			let partitions = SymbolClass<Int>([0x41...0x5A], [0x61...0x7A])
+			#expect(!partitions.contains(0x40))
+			#expect(partitions.contains(0x41))
+			#expect(partitions.contains(0x42))
+			#expect(partitions.contains(0x59))
+			#expect(partitions.contains(0x5A))
+			#expect(!partitions.contains(0x5B))
+			#expect(!partitions.contains(0x60))
+			#expect(partitions.contains(0x61))
+			#expect(partitions.contains(0x62))
+			#expect(partitions.contains(0x79))
+			#expect(partitions.contains(0x7A))
+			#expect(!partitions.contains(0x7B))
+		}
+		@Test("three ranges of values") func test_contains_ranges3() async throws {
+			let partitions = SymbolClass<Int>([0x30...0x39], [0x41...0x5A], [0x61...0x7A])
+			#expect(!partitions.contains(0x2F))
+			#expect(partitions.contains(0x30))
+			#expect(partitions.contains(0x31))
+			#expect(partitions.contains(0x38))
+			#expect(partitions.contains(0x39))
+			#expect(!partitions.contains(0x3A))
+			#expect(!partitions.contains(0x40))
+			#expect(partitions.contains(0x41))
+			#expect(partitions.contains(0x42))
+			#expect(partitions.contains(0x59))
+			#expect(partitions.contains(0x5A))
+			#expect(!partitions.contains(0x5B))
+			#expect(!partitions.contains(0x60))
+			#expect(partitions.contains(0x61))
+			#expect(partitions.contains(0x62))
+			#expect(partitions.contains(0x79))
+			#expect(partitions.contains(0x7A))
+			#expect(!partitions.contains(0x7B))
+		}
+	}
+	@Suite("get partition label") struct SymbolClassTests_getPartitionLabel {
+		@Test("three partitions") func test_three_parts() async throws {
+			let partitions = SymbolClass<Int>([0, 1, 2], [3, 4, 5], [6, 7, 8])
+			#expect(partitions.getPartitionLabel(0) == 0)
+			#expect(partitions.getPartitionLabel(1) == 0)
+			#expect(partitions.getPartitionLabel(2) == 0)
+			#expect(partitions.getPartitionLabel(3) == 3)
+			#expect(partitions.getPartitionLabel(4) == 3)
+			#expect(partitions.getPartitionLabel(5) == 3)
+			#expect(partitions.getPartitionLabel(6) == 6)
+			#expect(partitions.getPartitionLabel(7) == 6)
+			#expect(partitions.getPartitionLabel(8) == 6)
+		}
+		@Test("three ranges") func test_three_parts_ranges() async throws {
+			let partitions = SymbolClass<Int>([0...2], [3...4, 5...5], [6...7, 7...7, 8...8])
+			#expect(partitions.getPartitionLabel(0) == 0)
+			#expect(partitions.getPartitionLabel(1) == 0)
+			#expect(partitions.getPartitionLabel(2) == 0)
+			#expect(partitions.getPartitionLabel(3) == 3)
+			#expect(partitions.getPartitionLabel(4) == 3)
+			#expect(partitions.getPartitionLabel(5) == 3)
+			#expect(partitions.getPartitionLabel(6) == 6)
+			#expect(partitions.getPartitionLabel(7) == 6)
+			#expect(partitions.getPartitionLabel(8) == 6)
+		}
+		@Test("single symbol") func test_partitionReduce_single() async throws {
+			let partitions = SymbolClass<Int>([ 1 ])
+			#expect(partitions.contains(1))
+			#expect(!partitions.contains(2))
+		}
+		@Test("single set multi-symbol") func test_partitionReduce_multi() async throws {
+			let partitions = SymbolClass<Int>([0, 1, 2])
+			#expect(partitions.contains(0))
+			#expect(partitions.contains(1))
+			#expect(partitions.contains(2))
+			#expect(!partitions.contains(3))
+		}
+		@Test("range of values") func test_contains_range() async throws {
+			let partitions = SymbolClass<Int>(0x30...0x39)
+			#expect(!partitions.contains(0x2F))
+			#expect(partitions.contains(0x30))
+			#expect(partitions.contains(0x31))
+			#expect(partitions.contains(0x38))
+			#expect(partitions.contains(0x39))
+			#expect(!partitions.contains(0x3A))
+		}
+	}
+	@Suite("isEquivalent") struct SymbolClassTests_isEquivalent {
+		@Test("single symbol") func test__single() async throws {
+			let partitions = SymbolClass<Int>([ 1 ])
+			#expect(partitions.isEquivalent(1, 1))
+		}
+		@Test("single set multi-symbol") func test_isEquivalent_multi() async throws {
+			let partitions = SymbolClass<Int>([0, 1, 2])
+			#expect(partitions.isEquivalent(0, 1))
+		}
+	}
+	@Suite("meet") struct SymbolClassTests_meet {
+		@Test("two nested partitions") func test_two_nested_parts() async throws {
+			let part1 = SymbolClass<Int>([ 0x30...0x39 ])
+			let part2 = SymbolClass<Int>([ 0x32 ])
+			let partitions = part1.meet(part2)
+			#expect(partitions.getPartitionLabel(0x30) == 0x30)
+			#expect(partitions.getPartitionLabel(0x31) == 0x30)
+			#expect(partitions.getPartitionLabel(0x32) == 0x32)
+			#expect(partitions.getPartitionLabel(0x33) == 0x30)
+			#expect(partitions.getPartitionLabel(0x34) == 0x30)
+			#expect(partitions.getPartitionLabel(0x35) == 0x30)
+			#expect(partitions.getPartitionLabel(0x36) == 0x30)
+			#expect(partitions.getPartitionLabel(0x37) == 0x30)
+			#expect(partitions.getPartitionLabel(0x38) == 0x30)
+		}
+		@Test("two nested partitions (range)") func test_two_nested_parts_range() async throws {
+			let part1 = SymbolClass<Int>([ 0x30...0x39 ])
+			let part2 = SymbolClass<Int>([ 0x32...0x35 ])
+			let partitions = part1.meet(part2)
+			#expect(partitions.getPartitionLabel(0x30) == 0x30)
+			#expect(partitions.getPartitionLabel(0x31) == 0x30)
+			#expect(partitions.getPartitionLabel(0x32) == 0x32)
+			#expect(partitions.getPartitionLabel(0x33) == 0x32)
+			#expect(partitions.getPartitionLabel(0x34) == 0x32)
+			#expect(partitions.getPartitionLabel(0x35) == 0x32)
+			#expect(partitions.getPartitionLabel(0x36) == 0x30)
+			#expect(partitions.getPartitionLabel(0x37) == 0x30)
+			#expect(partitions.getPartitionLabel(0x38) == 0x30)
+		}
+		@Test("two overlapping partitions") func test_two_overlapping_parts() async throws {
+			let part1 = SymbolClass<Int>([ 0x30...0x35 ])
+			let part2 = SymbolClass<Int>([ 0x32...0x39 ])
+			let partitions = part1.meet(part2)
+			print(partitions.symbols)
+			#expect(partitions.getPartitionLabel(0x30) == 0x30)
+			#expect(partitions.getPartitionLabel(0x31) == 0x30)
+			#expect(partitions.getPartitionLabel(0x32) == 0x32)
+			#expect(partitions.getPartitionLabel(0x33) == 0x32)
+			#expect(partitions.getPartitionLabel(0x34) == 0x32)
+			#expect(partitions.getPartitionLabel(0x35) == 0x32)
+			#expect(partitions.getPartitionLabel(0x36) == 0x36)
+			#expect(partitions.getPartitionLabel(0x37) == 0x36)
+			#expect(partitions.getPartitionLabel(0x38) == 0x36)
+		}
+		@Test("two disjoint partitions") func test_two_disjoint_parts() async throws {
+			let part1 = SymbolClass<Int>([ 0x30...0x32 ])
+			let part2 = SymbolClass<Int>([ 0x35...0x39 ])
+			let partitions = part1.meet(part2)
+			#expect(partitions.getPartitionLabel(0x30) == 0x30)
+			#expect(partitions.getPartitionLabel(0x31) == 0x30)
+			#expect(partitions.getPartitionLabel(0x32) == 0x30)
+			#expect(!partitions.contains(0x33))
+			#expect(!partitions.contains(0x34))
+			#expect(partitions.getPartitionLabel(0x35) == 0x35)
+			#expect(partitions.getPartitionLabel(0x36) == 0x35)
+			#expect(partitions.getPartitionLabel(0x37) == 0x35)
+			#expect(partitions.getPartitionLabel(0x38) == 0x35)
+		}
+		@Test("3 partitions") func test_3_parts() async throws {
+			let part1 = SymbolClass<Int>([ 0x30...0x36 ])
+			let part2 = SymbolClass<Int>([ 0x32...0x34 ])
+			let part3 = SymbolClass<Int>([ 0x33...0x39 ])
+			let partitions = SymbolClass<Int>.meet([part1, part2, part3])
+			#expect(partitions.getPartitionLabel(0x30) == 0x30)
+			#expect(partitions.getPartitionLabel(0x31) == 0x30)
+			#expect(partitions.getPartitionLabel(0x32) == 0x32)
+			#expect(partitions.getPartitionLabel(0x33) == 0x33)
+			#expect(partitions.getPartitionLabel(0x34) == 0x33)
+			#expect(partitions.getPartitionLabel(0x35) == 0x35)
+			#expect(partitions.getPartitionLabel(0x36) == 0x35)
+			#expect(partitions.getPartitionLabel(0x37) == 0x37)
+			#expect(partitions.getPartitionLabel(0x38) == 0x37)
+			#expect(partitions.getPartitionLabel(0x39) == 0x37)
+		}
+		@Test("4 partitions") func test_4_parts() async throws {
+			let part1 = SymbolClass<Int>([ 0x30...0x36 ])
+			let part2 = SymbolClass<Int>([ 0x32...0x34 ])
+			let part3 = SymbolClass<Int>([ 0x33...0x38 ])
+			let part4 = SymbolClass<Int>([ 0x30...0x39 ])
+			let partitions = SymbolClass<Int>.meet([part1, part2, part3, part4])
+			#expect(partitions.getPartitionLabel(0x30) == 0x30)
+			#expect(partitions.getPartitionLabel(0x31) == 0x30)
+			#expect(partitions.getPartitionLabel(0x32) == 0x32)
+			#expect(partitions.getPartitionLabel(0x33) == 0x33)
+			#expect(partitions.getPartitionLabel(0x34) == 0x33)
+			#expect(partitions.getPartitionLabel(0x35) == 0x35)
+			#expect(partitions.getPartitionLabel(0x36) == 0x35)
+			#expect(partitions.getPartitionLabel(0x37) == 0x37)
+			#expect(partitions.getPartitionLabel(0x38) == 0x37)
+			#expect(partitions.getPartitionLabel(0x39) == 0x39)
 		}
 	}
 }
