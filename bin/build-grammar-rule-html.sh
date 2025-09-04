0e97ae4c6a2678fa7c0233455818acba2867f236
Austin Wright <aaa@bzfx.net>
2025-09-04 16:38:40 -0700

    Add optional line to build-grammar-rule-html.sh

--- bin/build-grammar-rule-html.sh
+++ bin/build-grammar-rule-html.sh
@@ -2,6 +2,8 @@
 set -ex
 for grammar_path in catalog/*.abnf; do
 	grammar=${grammar_path%%.abnf}
+	# Just in case you also want to generate the grammar index page
+	#bin/grammartool grammar-abnf-html $grammar_path > htdocs/$grammar.html
 	test -d htdocs/$grammar || mkdir htdocs/$grammar
 	for rulename in $(bin/grammartool abnf-list-rulenames $grammar_path); do
 		bin/grammartool grammar-abnf-rule-html $grammar_path $rulename > htdocs/$grammar/$rulename.html
