3d7133c9ac06ae56628a4f7feeb3552d4dd4c5b1
Austin Wright <aaa@bzfx.net>
2025-10-27 19:35:36 -0700

    Swift UI: Add some options for CFG view
    They don't do anything yet, though.

--- grammars/grammars/CFGView.swift
+++ grammars/grammars/CFGView.swift
@@ -6,6 +6,12 @@ struct CFGContentView: View {
 	public var charset: Charset;
 
 	@State private var selectedDialect: String = "bnf"
+	@State private var selectedRange: Bool = false
+	@State private var selectedEliminateUseless: Bool = true
+	@State private var selectedEliminateEpsilon: Bool = false
+	@State private var selectedForm: String = ""
+	@State private var selectedMatchType: String = "b"
+	@State private var selectedCharset: String = ""
 
 	var body: some View {
 		ScrollView {
@@ -13,6 +19,37 @@ struct CFGContentView: View {
 				Picker("Dialect", selection: $selectedDialect) {
 					Text("BNF").tag("bnf")
 				}
+
+				Toggle("Range operator", isOn: $selectedRange)
+
+				Toggle("Eliminate useless prodictions", isOn: $selectedEliminateEpsilon)
+
+				// TODO: If this is selected, and epsilon is in the language, then include epsilon in the set of start symbols.
+				// Because ordinarily "eliminate epsilon" removes it from the resulting language.
+				Toggle("Eliminate epsilon", isOn: $selectedEliminateUseless)
+
+				Picker("Normalize", selection: $selectedForm) {
+					Text("None").tag("")
+					Text("Chomsky").tag("c")
+					Text("Greibach").tag("g")
+				}
+				.pickerStyle(.segmented)
+
+				Picker("Charset", selection: $selectedCharset) {
+					Text("Preserve").tag("")
+					Text("UTF-8").tag("UTF-8")
+					Text("UTF-16").tag("UTF-16")
+					Text("UTF-32").tag("UTF-32")
+					Text("ISO-8859-1 (Latin-1)").tag("ISO-8859-1")
+				}
+
+				// TODO: Disable this if it seems like it wouldn't make a difference
+				Picker("Sort", selection: $selectedMatchType) {
+					Text("Breadth-first").tag("b")
+					Text("Depth-first").tag("d")
+					Text("Alphabetical").tag("a")
+				}
+				.pickerStyle(.segmented)
 			}
 			.pickerStyle(.menu)
 			.formStyle(.grouped)
