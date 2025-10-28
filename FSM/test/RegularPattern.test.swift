e1f411c56c8584dd86a38d8b2726cc49d66b7abb
Austin Wright <aaa@bzfx.net>
2025-10-28 14:25:11 -0700

    Swift: RegularPattern tests: Move sample values into test arguments

--- FSM/test/RegularPattern.test.swift
+++ FSM/test/RegularPattern.test.swift
@@ -6,43 +6,31 @@ import Testing;
 
 private protocol RegularPatternBuilderData: RegularPatternBuilder {
 	associatedtype FSM: DFAProtocol;
-	static var alpha: Self { get };
-	static var beta: Self { get };
 	var fsm: FSM { get };
 }
 
 extension ABNFAlternation: RegularPatternBuilderData {
 	typealias FSM = SymbolDFA<Symbol>;
-	static var alpha: ABNFAlternation<Symbol> { .symbol(1) }
-	static var beta: ABNFAlternation<Symbol> { .symbol(2) }
 	var fsm: SymbolDFA<Symbol> { try! self.toPattern() }
 }
 
 extension SymbolClassDFA: RegularPatternBuilderData where Alphabet.Symbol: BinaryInteger, Alphabet.Element == SymbolClass {
 	typealias FSM = SymbolClassDFA<Alphabet>;
-	static var alpha: SymbolClassDFA<Alphabet> { .symbol(1) }
-	static var beta: SymbolClassDFA<Alphabet> { .symbol(2) }
 	var fsm: FSM { self.toPattern() }
 }
 
 extension SymbolClassNFA: RegularPatternBuilderData where Alphabet.Symbol: BinaryInteger {
 	typealias FSM = SymbolClassDFA<Alphabet>;
-	static var alpha: SymbolClassNFA<Alphabet> { .symbol(1) }
-	static var beta: SymbolClassNFA<Alphabet> { .symbol(2) }
 	var fsm: FSM { self.toDFA().fsm }
 }
 
 extension REPattern<UInt8>: RegularPatternBuilderData {
 	typealias FSM = SymbolDFA<UInt8>;
-	static var alpha: REPattern<UInt8> { .symbol(1) }
-	static var beta: REPattern<UInt8> { .symbol(2) }
 	var fsm: SymbolDFA<UInt8> { self.toPattern() }
 }
 
 extension SimpleRegex: RegularPatternBuilderData {
 	typealias FSM = SymbolDFA<Symbol>;
-	static var alpha: SimpleRegex<Symbol> { .symbol(1) }
-	static var beta: SimpleRegex<Symbol> { .symbol(2) }
 	var fsm: SymbolDFA<Symbol> { self.toPattern() }
 }
 
@@ -50,13 +38,13 @@ private struct RegularPatternBuilderTests {
 	// "Pattern Type"
 	struct PT: CustomDebugStringConvertible {
 		var type: any RegularPatternBuilderData.Type
+		var alpha: any RegularPatternBuilderData
+		var beta: any RegularPatternBuilderData
 		var debugDescription: String { "\(type)" }
-//		var alpha: any RegularPatternBuilderData
-//		var beta: any RegularPatternBuilderData
 		init<T: RegularPatternBuilderData>(_ type: T.Type, _ alpha: T, _ beta: T) {
 			self.type = type
-//			self.alpha = alpha
-//			self.beta = beta
+			self.alpha = alpha
+			self.beta = beta
 		}
 	}
 
@@ -65,6 +53,7 @@ private struct RegularPatternBuilderTests {
 		PT(SymbolDFA<UInt8>.self, .symbol(1), .symbol(2)),
 		PT(SymbolClassNFA<SymbolAlphabet<UInt8>>.self, .symbol(1), .symbol(2)),
 		PT(RangeDFA<UInt8>.self, .symbol(1), .symbol(2)),
+		PT(RangeDFA<UInt8>.self, .symbol(range: [0...3, 8...9]), .symbol(range: [4...7])),
 		PT(REPattern<UInt8>.self, .symbol(1), .symbol(2)),
 		PT(SimpleRegex<UInt8>.self, .symbol(1), .symbol(2)),
 	];
@@ -72,14 +61,16 @@ private struct RegularPatternBuilderTests {
 	@Test(arguments: RegularPatternBuilderTests.allTests)
 	func toPattern_empty(_ helper: PT) {
 		func test<T: RegularPatternBuilderData>(_ type: T.Type) {
+			let alpha = helper.alpha as! T
+			let beta = helper.beta as! T
 			#expect(T.empty == T.empty)
 			#expect(T.empty != T.epsilon)
-			#expect(T.empty != T.alpha)
-			#expect(T.empty != T.beta)
+			#expect(T.empty != alpha)
+			#expect(T.empty != beta)
 			#expect(T.empty.fsm == T.empty.fsm)
 			#expect(T.empty.fsm != T.epsilon.fsm)
-			#expect(T.empty.fsm != T.alpha.fsm)
-			#expect(T.empty.fsm != T.beta.fsm)
+			#expect(T.empty.fsm != alpha.fsm)
+			#expect(T.empty.fsm != beta.fsm)
 		}
 		test(helper.type)
 	}
@@ -87,14 +78,16 @@ private struct RegularPatternBuilderTests {
 	@Test(arguments: RegularPatternBuilderTests.allTests)
 	func test_epsilon(_ helper: PT) {
 		func test<T: RegularPatternBuilderData>(_ type: T.Type) {
+			let alpha = helper.alpha as! T
+			let beta = helper.beta as! T
 			#expect(T.epsilon != T.empty)
 			#expect(T.epsilon == T.epsilon)
-			#expect(T.epsilon != T.alpha)
-			#expect(T.epsilon != T.beta)
+			#expect(T.epsilon != alpha)
+			#expect(T.epsilon != beta)
 			#expect(T.epsilon.fsm != T.empty.fsm)
 			#expect(T.epsilon.fsm == T.epsilon.fsm)
-			#expect(T.epsilon.fsm != T.alpha.fsm)
-			#expect(T.epsilon.fsm != T.beta.fsm)
+			#expect(T.epsilon.fsm != alpha.fsm)
+			#expect(T.epsilon.fsm != beta.fsm)
 		}
 		test(helper.type)
 	}
@@ -102,22 +95,24 @@ private struct RegularPatternBuilderTests {
 	@Test(arguments: RegularPatternBuilderTests.allTests)
 	func test_data(_ helper: PT) {
 		func test<T: RegularPatternBuilderData>(_ type: T.Type) {
-			#expect(T.alpha != T.empty)
-			#expect(T.alpha != T.epsilon)
-			#expect(T.alpha == T.alpha)
-			#expect(T.alpha != T.beta)
-			#expect(T.alpha.fsm != T.empty.fsm)
-			#expect(T.alpha.fsm != T.epsilon.fsm)
-			#expect(T.alpha.fsm == T.alpha.fsm)
-			#expect(T.alpha.fsm != T.beta.fsm)
-			#expect(T.beta != T.empty)
-			#expect(T.beta != T.epsilon)
-			#expect(T.beta != T.alpha)
-			#expect(T.beta == T.beta)
-			#expect(T.beta.fsm != T.empty.fsm)
-			#expect(T.beta.fsm != T.epsilon.fsm)
-			#expect(T.beta.fsm != T.alpha.fsm)
-			#expect(T.beta.fsm == T.beta.fsm)
+			let alpha = helper.alpha as! T
+			let beta = helper.beta as! T
+			#expect(alpha != T.empty)
+			#expect(alpha != T.epsilon)
+			#expect(alpha == alpha)
+			#expect(alpha != beta)
+			#expect(alpha.fsm != T.empty.fsm)
+			#expect(alpha.fsm != T.epsilon.fsm)
+			#expect(alpha.fsm == alpha.fsm)
+			#expect(alpha.fsm != beta.fsm)
+			#expect(beta != T.empty)
+			#expect(beta != T.epsilon)
+			#expect(beta != alpha)
+			#expect(beta == beta)
+			#expect(beta.fsm != T.empty.fsm)
+			#expect(beta.fsm != T.epsilon.fsm)
+			#expect(beta.fsm != alpha.fsm)
+			#expect(beta.fsm == beta.fsm)
 		}
 		test(helper.type)
 	}
@@ -125,13 +120,15 @@ private struct RegularPatternBuilderTests {
 	@Test(arguments: RegularPatternBuilderTests.allTests)
 	func test_union(_ helper: PT) {
 		func test<T: RegularPatternBuilderData>(_ type: T.Type) {
+			let alpha = helper.alpha as! T
+			let beta = helper.beta as! T
 			#expect(T.empty.union(T.empty).fsm == T.empty.fsm)
 			#expect(T.empty.union(T.epsilon).fsm == T.epsilon.fsm)
 			#expect(T.epsilon.union(T.empty).fsm == T.epsilon.fsm)
 			#expect(T.epsilon.union(T.epsilon).fsm == T.epsilon.fsm)
-			#expect(T.alpha.union(T.empty).fsm == T.alpha.fsm)
-			#expect(T.alpha.union(T.epsilon).fsm == T.alpha.fsm.union(T.epsilon.fsm))
-			#expect(T.alpha.union(T.alpha).fsm == T.alpha.fsm)
+			#expect(alpha.union(T.empty).fsm == alpha.fsm)
+			#expect(alpha.union(T.epsilon).fsm == alpha.fsm.union(T.epsilon.fsm))
+			#expect(alpha.union(alpha).fsm == alpha.fsm)
 		}
 		test(helper.type)
 	}
@@ -139,14 +136,16 @@ private struct RegularPatternBuilderTests {
 	@Test(arguments: RegularPatternBuilderTests.allTests)
 	func test_concatenate(_ helper: PT) {
 		func test<T: RegularPatternBuilderData>(_ type: T.Type) {
+			let alpha = helper.alpha as! T
+			let beta = helper.beta as! T
 			#expect(T.empty.concatenate(T.empty).fsm == T.empty.fsm)
 			#expect(T.empty.concatenate(T.epsilon).fsm == T.empty.fsm)
 			#expect(T.epsilon.concatenate(T.empty).fsm == T.empty.fsm)
 			#expect(T.epsilon.concatenate(T.epsilon).fsm == T.epsilon.fsm)
-			#expect(T.alpha.concatenate(T.empty).fsm == T.empty.fsm)
-			#expect(T.alpha.concatenate(T.epsilon).fsm == T.alpha.fsm)
-			#expect(T.alpha.concatenate(T.epsilon).concatenate(T.beta).fsm == T.alpha.fsm.concatenate(T.beta.fsm))
-			#expect(T.alpha.concatenate(T.alpha).fsm == T.concatenate([ T.alpha, T.alpha ]).fsm)
+			#expect(alpha.concatenate(T.empty).fsm == T.empty.fsm)
+			#expect(alpha.concatenate(T.epsilon).fsm == alpha.fsm)
+			#expect(alpha.concatenate(T.epsilon).concatenate(beta).fsm == alpha.fsm.concatenate(beta.fsm))
+			#expect(alpha.concatenate(alpha).fsm == T.concatenate([ alpha, alpha ]).fsm)
 		}
 		test(helper.type)
 	}
@@ -154,9 +153,11 @@ private struct RegularPatternBuilderTests {
 	@Test(arguments: RegularPatternBuilderTests.allTests)
 	func test_star(_ helper: PT) {
 		func test<T: RegularPatternBuilderData>(_ type: T.Type) {
+			let alpha = helper.alpha as! T
+			let beta = helper.beta as! T
 			#expect(T.empty.star().fsm == T.empty.fsm.star())
 			#expect(T.epsilon.star().fsm == T.epsilon.fsm.star())
-			#expect(T.alpha.star().fsm == T.alpha.fsm.star())
+			#expect(alpha.star().fsm == alpha.fsm.star())
 		}
 		test(helper.type)
 	}
