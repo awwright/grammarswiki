c6ca077138da94d64248526200519763b862c814
Austin Wright <aaa@bzfx.net>
2026-01-27 14:22:01 -0700

    Swift: Dereference pointers to external files in Catalog.load

--- FSM/lib/Catalog.swift
+++ FSM/lib/Catalog.swift
@@ -23,10 +23,13 @@ public struct Catalog {
 	public func load<T>(path: String, rulename: String? = nil) throws
 		-> (rules: ABNFRulelist<T>, backward: Dictionary<String, (filename: String, ruleid: String)>)
 	{
-		// Keep a list of imported files
-		var importedFiles: Dictionary<String, ABNFRulelist<T>> = [:];
-
 		let (root_rulelist, root_backwards) = try dereference(source_filename: path)
+		// Keep a list of imported files
+		var all_dereferenced = [path: (root_rulelist, root_backwards)];
+		// Keep track of the order the files were loaded in
+		var all_path_priority = [path];
+		var all_mangled: Dictionary<String, ABNFRulelist<T>> = [path: root_rulelist];
+		var all_backwards = root_backwards;
 
 		// Make the list of rules to be included in the resulting output
 		let root_rulenames: Array<String>;
@@ -52,6 +55,7 @@ public struct Catalog {
 				rulelist
 				.mapRulenames {
 					let mangled = "{File: \(source_filename) Rule: \($0.label.lowercased())}";
+					backwards[mangled] = (source_filename, $0.label.lowercased());
 					return ABNFRulename<T>(id: mangled, label: mangled); // rulename.label
 				}
 				.mapElements {
@@ -74,24 +78,46 @@ public struct Catalog {
 
 		// Load only the desired rules
 		var required_rulelist = root_rulelist.rules.filter { root_rulenames.contains($0.rulename.id) };
+		// Keep track of rules that have been added to the list
 		var required_rulelist_set = Set(required_rulelist.map { $0.rulename.id });
 
 		// Iterate through the rulelist, appending required rules as needed
 		var i = 0;
 		while i < required_rulelist.count {
 			let r = required_rulelist[i];
+			// Get a list of the rules that this rule refers to, and add those to the list as necessary
 			for ref in r.referencedRules.sorted() {
-				if !required_rulelist_set.contains(ref) {
-					// Determine which file we find the reference in
-					for new_r in root_rulelist.rules.filter({ $0.rulename.id == ref }) {
-						required_rulelist.append(new_r);
-						required_rulelist_set.insert(new_r.rulename.id);
-					}
+				// Skip rules we have already loaded
+				guard !required_rulelist_set.contains(ref) else { continue }
+
+				// If the rule is from an external file, make sure it's loaded
+				guard let (required_filename, required_rulename) = root_backwards[ref]
+				else { continue; }
+
+				if all_dereferenced[required_filename] == nil {
+					let (ref_rulelist, ref_backwards) = try dereference(source_filename: required_filename);
+					all_dereferenced[required_filename] = (ref_rulelist, ref_backwards);
+					all_mangled[path] = ref_rulelist;
+					for (k, v) in ref_backwards { all_backwards[k] = v; }
 				}
+
+				let (rulelist, _) = all_dereferenced[required_filename]!;
+
+				// Determine which file we find the reference in
+				for new_r in rulelist.rules.filter({ $0.rulename.id == ref }) {
+					required_rulelist.append(new_r);
+					required_rulelist_set.insert(new_r.rulename.id);
+				}
+
 			}
 			i += 1;
 		}
 
-		return (rules: ABNFRulelist<T>(rules: required_rulelist), backward: root_backwards)
+		// Now the rulenames in required_rulelist and all_backwards are unique... but also illegal.
+		// Map them to their original rulenames, when they're unambiguous.
+		let rules = ABNFRulelist<T>(rules: required_rulelist).mapRulenames { ABNFRulename(label: all_backwards[$0.label]!.ruleid) };
+		let backward = all_backwards.filter { required_rulelist_set.contains($0.0) };
+
+		return (rules: rules, backward: backward)
 	}
 }
