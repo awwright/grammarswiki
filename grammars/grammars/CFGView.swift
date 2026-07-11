901b43bae06498c3a085f82435bf33433b889d3a
Austin Wright <aaa@bzfx.net>
2026-07-11 08:52:16 -0700

    Swift UI: Render CFG BNF using AttributedString
    This is much more performant than stacking together lots of views and long lines wrap correctly

--- grammars/grammars/CFGView.swift
+++ grammars/grammars/CFGView.swift
@@ -30,7 +30,7 @@ struct CFGContentView: View {
 	@State private var selectedCharset: String = ""
 	@State private var selectedDialect: CFGContentView_Dialect = .bnf
 
-	func filteredGrammar() -> ABNFRulelist<UInt32>.CFG {
+	var filteredGrammar: ABNFRulelist<UInt32>.CFG {
 		var grammar = self.grammar;
 		if selectedEliminateUseless { grammar = grammar.eliminateUseless(); }
 		if selectedEliminateEpsilon { grammar = grammar.eliminateEpsilon(); }
@@ -40,8 +40,16 @@ struct CFGContentView: View {
 		return grammar;
 	}
 
+	var sortedRules: Array<ABNFRulelist<UInt32>.CFG.Variable> {
+		switch selectedSortOrder {
+			case .breadthFirst: grammar.ruleNames;
+			case .depthFirst: grammar.ruleNamesDepthFirst;
+			case .name: grammar.ruleNames.sorted();
+		}
+	}
+
 	var body: some View {
-		let grammar = self.filteredGrammar();
+		let grammar = self.filteredGrammar;
 		ScrollView {
 			Form {
 				Toggle("Eliminate useless prodictions", isOn: $selectedEliminateUseless)
@@ -76,8 +84,8 @@ struct CFGContentView: View {
 				// TODO: collapse tail recursive rules like:
 				// rule = epsilon / element rule
 				// into: rule = element*
-				Toggle("Kleene star operator", isOn: $selectedRange)
-				Toggle("Optional group operator", isOn: $selectedRange)
+				//Toggle("Kleene star operator", isOn: $selectedRange)
+				//Toggle("Optional group operator", isOn: $selectedRange)
 
 				Picker("Dialect", selection: $selectedDialect) {
 					Text("BNF").tag(CFGContentView_Dialect.bnf)
@@ -90,9 +98,9 @@ struct CFGContentView: View {
 			VStack(alignment: .leading) {
 				switch selectedDialect {
 				case .bnf:
-					CFGContentView_BNF(grammar: grammar, selectedEliminateEpsilon: selectedEliminateEpsilon, selectedSortOrder: selectedSortOrder)
+					CFGContentView_BNF(grammar: grammar, ruleNames: self.sortedRules, selectedEliminateEpsilon: selectedEliminateEpsilon)
 				case .swift_cfg:
-					CFGContentView_SwiftCFG(grammar: grammar, selectedSortOrder: selectedSortOrder)
+					CFGContentView_SwiftCFG(grammar: grammar, ruleNames: self.sortedRules)
 				}
 			}
 			.frame(maxWidth: .infinity, alignment: .topLeading)
@@ -102,60 +110,97 @@ struct CFGContentView: View {
 
 struct CFGContentView_BNF: View {
 	let grammar: ABNFRulelist<UInt32>.CFG
+	let ruleNames: Array<ABNFRulelist<UInt32>.CFG.Variable>
 	let selectedEliminateEpsilon: Bool
-	let selectedSortOrder: CFGContentView_SortOrder
 
 	@Environment(SelectedCharset.self) private var charset;
 
 	var body: some View {
-		ForEach(grammar.start, id: \.self) { rulename in
-			Text("\u{2192} \(rulename)")
-		}
-		if selectedEliminateEpsilon && self.grammar.contains([]) {
-			// If the "eliminate epsilon productions" option removed epsilon from the language, add it back here
-			Text("\u{2192} \u{3B5}") // Epsilon
-		}
-		if grammar.start.isEmpty {
-			Text("\u{2192} \u{2205}")
-		}
-		Spacer()
-		let dictionary = grammar.dictionary
-		let ruleNames = switch(selectedSortOrder) {
-			case .breadthFirst: grammar.ruleNames;
-			case .depthFirst: grammar.ruleNamesDepthFirst;
-			case .name: grammar.ruleNames.sorted();
-		}
-		ForEach(ruleNames, id: \.self) { (ruleName: ABNFRulelist<UInt32>.CFG.Variable) in
-			let rules = dictionary[ruleName] ?? []
-			Text("\(ruleName)").font(.headline);
-			if rules.isEmpty {
-				Text("\t= \u{2205}");
+		Text(generatedCode)
+			.textSelection(.enabled)
+			.frame(maxWidth: .infinity, alignment: .topLeading)
+	}
+
+	private var generatedCode: AttributedString {
+		var result = AttributedString();
+
+		// Start symbols
+		if !grammar.start.isEmpty {
+			for rulename in grammar.start {
+				var arrow = AttributedString("\u{2192}");
+				arrow.font = .headline;
+				result += arrow + " " + AttributedString(rulename.description) + "\n";
 			}
-			ForEach(rules, id: \.self) { rule in
-				// FIXME: This will squish rather than wrap
-				HStack {
-					Text("\t\u{2192} ")
-					ForEach(rule.body, id: \.self) { (token: ABNFRulelist<UInt32>.CFG.BodyElement) in
-						switch token {
-							case .terminal(let sym): Text(charset.describe(sym)).monospaced()
-							case .nonterminal(let name): Text(name.description)
-							default: Text(String(describing: token))
-						}
-					}
-					if rule.body.isEmpty {
-						Text("\u{3B5}") // Epsilon
-					}
-				}
-			}
-			Spacer()
+		} else {
+			var empty = AttributedString("\u{2192} \u{2205}\n");
+			empty.foregroundColor = .secondary;
+			result += empty;
 		}
 
+		// Show epsilon, if it was eliminated earlier
+		if selectedEliminateEpsilon && self.grammar.contains([]) {
+			var epsilon = AttributedString("\u{2192} \u{3B5}\n");
+			epsilon.foregroundColor = .orange;
+			result += epsilon;
+		}
+		// Empty line
+		result += "\n";
+
+		let dictionary = grammar.dictionary
+
+		for ruleName in ruleNames {
+			let rules = dictionary[ruleName] ?? [];
+
+			// Rule name (headline)
+			var namePart = AttributedString(ruleName.description);
+			namePart.font = .headline;
+			result += namePart + "\n";
+
+			if rules.isEmpty {
+				result += "\u{2192} \u{2205}\n";
+				continue;
+			}
+
+			for rule in rules {
+				var arrow = AttributedString("\t\u{2192}")
+				arrow.foregroundColor = .secondary
+				result += arrow
+
+				if rule.body.isEmpty {
+					var eps = AttributedString(" \u{3B5}\n")
+					eps.foregroundColor = .orange
+					result += eps
+				} else {
+					for token in rule.body {
+						switch token {
+						case .terminal(let sym):
+							var term = AttributedString(charset.describe(sym));
+							term.font = .body.monospaced();
+							term.foregroundColor = .purple;
+							result += " " + term;
+
+						case .nonterminal(let name):
+							result += " " + AttributedString(name.description);
+
+						default:
+							result += " " + AttributedString(String(describing: token));
+						}
+					}
+					result += "\n";
+				}
+			}
+
+			// Empty line between rules
+			result += "\n";
+		}
+
+		return result;
 	}
 }
 
 struct CFGContentView_SwiftCFG: View {
 	let grammar: ABNFRulelist<UInt32>.CFG
-	let selectedSortOrder: CFGContentView_SortOrder
+	let ruleNames: Array<ABNFRulelist<UInt32>.CFG.Variable>
 
 	var body: some View {
 		Text(generatedCode)
@@ -171,11 +216,6 @@ struct CFGContentView_SwiftCFG: View {
 		}
 
 		let dictionary = grammar.dictionary;
-		let ruleNames: [ABNFRulelist<UInt32>.CFG.Variable] = switch selectedSortOrder {
-			case .breadthFirst: grammar.ruleNames
-			case .depthFirst: grammar.ruleNamesDepthFirst
-			case .name: grammar.ruleNames.sorted()
-		}
 
 		var lines: [String] = [];
 		lines.append("ABNFRulelist<UInt32>.CFG(");
